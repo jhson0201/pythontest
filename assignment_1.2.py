bf6ff0142b17d0d0964bed46944a3c439d20fd60
jhson0201 <jhson0201@naver.com>
2017-01-15 18:37:35 +0800

    Add assignment_1.2.py

---
+++ assignment_1.2.py
@@ -0,0 +1,18 @@
+#
+# Class user:
+#     def_init_(self, name, gender, age, position):
+#     self.name=name
+#     self.gender=gender
+#     self.age=age
+#     self.position=position
+#
+# Class partner(user):
+#     position="대리"
+#
+# name=input("이름을 입력하세요 ")
+# gender=input("성별을 입력하세요 ")
+# age=input("나이를 입력하세요")
+# position=input("직급을 입력하세요")
+#
+# a=user(name, gender, age, position)
+# print(a._dict_)
